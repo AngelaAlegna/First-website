1dcb031127787742b1b7dd9124f06acd79a5ac51
AngelaAlegna <44700338+AngelaAlegna@users.noreply.github.com>
2018-11-04 15:40:17 +1000

    Image gallery and more

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -31,3 +31,14 @@ function activeMenu() {
 }
 
 activeMenu();
+
+// image gallery
+
+function imageGallery() {
+
+    if (!$('.image-gallery').length) {
+        return;
+    }
+$('.image-gallery a').simpleLightbox();
+}
+imageGallery();
